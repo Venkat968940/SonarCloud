0c14cc5b80257d50fe47898f4e74dbde82f148c9
Venkatesh@instrive <venkatesh@instrive.in>
2025-12-30 15:57:40 +0530

    sonar cloud test

--- src/BadExample.tsx
+++ src/BadExample.tsx
@@ -11,9 +11,6 @@ const BadExample = () => {
   // ❌ duplicate code
   console.log("duplicate");
   console.log("duplicate");
-  console.log(
-    "sendgridkey => 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"
-  );
 
   return <div>Bad Example</div>;
 };
