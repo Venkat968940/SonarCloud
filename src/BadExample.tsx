c177a37baeab163f40d3c785154eb9619995c0d4
Venkatesh@instrive <venkatesh@instrive.in>
2025-12-30 15:59:37 +0530

    test

--- src/BadExample.tsx
+++ src/BadExample.tsx
@@ -11,7 +11,9 @@ const BadExample = () => {
   // ❌ duplicate code
   console.log("duplicate");
   console.log("duplicate");
-
+  console.log(
+    "sendgridkey => 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"
+  );
   return <div>Bad Example</div>;
 };
 
